f8a7c5b5c8f30e2a81191a1a6f9a70efbf3a8d01
Enio Maxson <enio.maxson@gmail.com>
2023-12-19 20:44:15 +0000

    adding a new file to map the endpoints

---
+++ src/MyBank.API/MyBankEndpointsV1.cs
@@ -0,0 +1,65 @@
+﻿using MyBank.Domain;
+using System.Collections.Concurrent;
+
+namespace MyBank.API
+{
+    public static class MyBankEndpointsV1
+    {
+        public static ConcurrentDictionary<string, BankAccount> database = new();
+
+        public static void MapMyBankEndpoints(this WebApplication app)
+        {
+            app.MapGet("", HandleAccountList);
+            app.MapPost("/open", HandleOpenAccount);
+            app.MapGet("/{accountNumber}/detail", HandleAccountDetail);
+            app.MapPut("/{accountNumber}/deposit", HandleDeposit);
+            app.MapPut("/{accountNumber}/withdraw", HandleWithdrawl);
+        }
+
+        private static IResult HandleAccountDetail(string accountNumber)
+        {
+            if (database.TryGetValue(accountNumber, out var bankAccount))
+            {
+                return Results.Ok(bankAccount);
+            }
+
+            return Results.NotFound();
+        }
+
+        private static IResult HandleAccountList()
+        {
+            return Results.Ok(database.Values);
+        }
+
+        private static IResult HandleDeposit(string accountNumber, DepositCommand command)
+        {
+            if (database.TryGetValue(accountNumber, out var bankAccount))
+            {
+                bankAccount.Deposit(command.Ammount, command.Note);
+                return Results.Accepted();
+            }
+
+            return Results.BadRequest();
+        }
+
+        private static IResult HandleWithdrawl(string accountNumber, DepositCommand command)
+        {
+            if (database.TryGetValue(accountNumber, out var bankAccount))
+            {
+                bankAccount.Withdrawal(command.Ammount, command.Note);
+                return Results.Accepted();
+            }
+
+            return Results.BadRequest();
+        }
+
+        private static IResult HandleOpenAccount(OpenAccountCommand command)
+        {
+            var bankAccount = new BankAccount(command.Owner, command.initialDeposit);
+
+            database.TryAdd(bankAccount.Number, bankAccount);
+
+            return Results.Created($"/{bankAccount.Number}/detail", bankAccount);
+        }
+    }
+}
--- src/MyBank.API/Program.cs
+++ src/MyBank.API/Program.cs
@@ -1,6 +1,4 @@
 using MyBank.API;
-using MyBank.Domain;
-using System.Collections.Concurrent;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,52 +16,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-ConcurrentDictionary<string, BankAccount> database = new();
-
-app.MapGet("/", () =>
-{
-    return Results.Ok(database.Values);
-});
-
-app.MapPost("/open", (OpenAccountCommand command) =>
-{
-    var bankAccount = new BankAccount(command.Owner, command.initialDeposit);
-
-    database.TryAdd(bankAccount.Number, bankAccount);
-
-    return Results.Created($"/{bankAccount.Number}/detail", bankAccount);
-});
-
-app.MapGet("/{accountNumber}/detail", (string accountNumber) =>
-{
-    if (database.TryGetValue(accountNumber, out var bankAccount))
-    {
-        return Results.Ok(bankAccount);
-    }
-
-    return Results.NotFound();
-});
-
-app.MapPut("/{accountNumber}/deposit", (string accountNumber, DepositCommand command) =>
-{
-    if (database.TryGetValue(accountNumber, out var bankAccount))
-    {
-        bankAccount.Deposit(command.Ammount, command.Note);
-        return Results.Accepted();
-    }
-
-    return Results.BadRequest();
-});
-
-app.MapPut("/{accountNumber}/withdrawl", (string accountNumber, DepositCommand command) =>
-{
-    if (database.TryGetValue(accountNumber, out var bankAccount))
-    {
-        bankAccount.Withdrawal(command.Ammount, command.Note);
-        return Results.Accepted();
-    }
-
-    return Results.BadRequest();
-});
+app.MapMyBankEndpoints();
 
 app.Run();
